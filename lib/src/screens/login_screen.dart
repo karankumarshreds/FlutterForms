f2d0c68e2ae275526b731fb78c721e64100695a6
karankumarshreds <naileditnerd@gmail.com>
2021-07-23 13:49:52 +0530

    add login screen boilerplate

---
+++ lib/src/screens/login_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  @override
+  createState() => LoginScreenState();
+}
+
+class LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(context) {
+    return Text('Login Screen');
+  }
+}
